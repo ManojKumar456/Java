37c7f4eb949914670a551e62a70a873fcc042048
mk.java88 <mk.java88@gmail.com>
2018-08-16 09:43:31 -0400

    2nd

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 public static void main(String[] args) {
 	System.out.println("Hello World");
-	System.out.println("Hello World 1st edited version");
+	System.out.println("Hello World 2nd edited version");
 
 
 i
