60b8afca27542c1644db19d951d76dd74e5712c6
kumar24.java@gmail.com <kumar24.java@gmail.com>
2018-08-16 14:23:24 -0400

    8th

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,15 +4,13 @@ public static void main(String[] args) {
 	System.out.println("Hello World");
 
 	System.out.println("Hello World 2nd edited version");
-	        System.out.println("Hello World 3rd edited version");
-
-         System.out.println("Hello World 2");
-
-	 System.out.println("Hello World 4th");
-	 
-         System.out.println("Hello World 5th");
-	 System.out.println("Hello World 6th");
-	          System.out.println("Hello World 7th");
+	System.out.println("Hello World 3rd edited version");
+        System.out.println("Hello World 2");
+        System.out.println("Hello World 4th");
+        System.out.println("Hello World 5th");
+        System.out.println("Hello World 6th");
+        System.out.println("Hello World 7th");
+        System.out.println("Hello World 8th");
 i
 }
 }
